7692f54994d1961a33ffb11bdce88303649b8970
arifi083 <atifarif083@gmail.com>
2022-02-10 20:07:32 -0800

    vue js route

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+ <template>
+  <div class="about">
+    
+    <h2>This is Home Page</h2>
+    <h2>This is our first Page</h2>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ 
+</style>
